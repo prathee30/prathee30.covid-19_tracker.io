c0274708a08fa1d289801487f6dd9d321e458c26
prathee <pratheekshavenkatachalam@gmail.com>
2020-07-06 23:53:56 +0530

    Updates

---
+++ precache-manifest.75caaa9871ebd5ae264a192a32cc6c7c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b14e3dc3dced97bf12cec8c16b6807c",
+    "url": "/covid-19_tracker/index.html"
+  },
+  {
+    "revision": "b38ce56dcc07a01200c4",
+    "url": "/covid-19_tracker/static/css/main.9ae777de.chunk.css"
+  },
+  {
+    "revision": "b52c80b62e64c75fd1e4",
+    "url": "/covid-19_tracker/static/js/2.0d947508.chunk.js"
+  },
+  {
+    "revision": "98e7206d2472a9ba0c2c6593c9913f5f",
+    "url": "/covid-19_tracker/static/js/2.0d947508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b38ce56dcc07a01200c4",
+    "url": "/covid-19_tracker/static/js/main.9bc26b94.chunk.js"
+  },
+  {
+    "revision": "39dfc08d23df11426c0a",
+    "url": "/covid-19_tracker/static/js/runtime-main.ce6a481a.js"
+  },
+  {
+    "revision": "fd59fe9ea4258192839496ed65af7086",
+    "url": "/covid-19_tracker/static/media/image.fd59fe9e.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19_tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
